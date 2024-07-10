da64b742d2930084d5db941c4c2089a44f33b22a
Paul Patterson <pazerp@hotmail.com>
2024-07-10 14:14:53 +0100

    Initial Commit

---
+++ GenericSection/GenericSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericSectionApp.swift
+//  GenericSection
+//
+//  Created by Paul Patterson on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenericSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
